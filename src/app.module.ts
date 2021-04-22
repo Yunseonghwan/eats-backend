380d7534f5d1570e9f43522a19f60178f915a422
Yunseonghwan <sunghwan0208@naver.com>
2021-04-23 02:11:13 +0900

    db연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { RestaurnatsModule } from './restaurnats/restaurnats.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === "dev" ? ".dev.env" : ".test.env",
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username:'yun',
+      password: 'dml134679',
+      database: 'eats',
+      synchronize: true,
+      logging: true
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true
     }),
-    RestaurnatsModule
+    RestaurnatsModule,
+   
   ],
   controllers: [],
   providers: [],
