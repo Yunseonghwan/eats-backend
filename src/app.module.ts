ae55be6f90aa9b565ae2e892a16230a56c1ab6cc
Yunseonghwan <sunghwan0208@naver.com>
2021-04-23 03:02:16 +0900

    env 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,36 @@
 import { Module } from '@nestjs/common';
+import * as Joi from 'joi'; // js용 스키마설명언어 이자 데이터 유효성 검사툴
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
-import { TypeOrmModule } from '@nestjs/typeorm'
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurnatsModule } from './restaurnats/restaurnats.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: process.env.NODE_ENV === "dev" ? ".dev.env" : ".test.env",
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.dev.env' : '.test.env',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('dev', 'prod')
+          .required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+      }),
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username:'yun',
-      password: 'dml134679',
-      database: 'eats',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       synchronize: true,
-      logging: true
+      logging: true,
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: true
