53a5284179d83011e6eb0b9ff201660b5dff0b5d
Yunseonghwan <sunghwan0208@naver.com>
2021-05-10 23:22:46 +0900

    create account test

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -7,11 +7,11 @@ import { User } from "./entities/user.entity";
 import { Verification } from "./entities/verification.entity";
 import { UsersService } from "./users.service"
 
-const mockRepository = {
+const mockRepository = () => ({
     findOne: jest.fn(),
     save: jest.fn(),
     create: jest.fn(),
-};
+});
 
 const mockJwtService = {
     sign: jest.fn(),
@@ -28,6 +28,8 @@ describe("UserService", () => {
 
     let service: UsersService;
     let usersRepository: mockRepository<User>;
+    let verificationsRepository: mockRepository<Verification>
+    let mailService: MailService
 
     
     beforeAll(async () => {
@@ -35,11 +37,11 @@ describe("UserService", () => {
            providers: [UsersService,
             {
                provide: getRepositoryToken(User), 
-               useValue: mockRepository,
+               useValue: mockRepository(),
             },
             {
                 provide: getRepositoryToken(Verification), 
-                useValue: mockRepository,
+                useValue: mockRepository(),
             },
             {
                 provide: JwtService, 
@@ -52,7 +54,9 @@ describe("UserService", () => {
         ], 
         }).compile();
         service = module.get<UsersService>(UsersService);
+        mailService = module.get<MailService>(MailService)
         usersRepository = module.get(getRepositoryToken(User))
+        verificationsRepository = module.get(getRepositoryToken(Verification));
     });
 
     it("should be defined", () => {
@@ -60,21 +64,47 @@ describe("UserService", () => {
     })
 
     describe('createAccount', () => {
+        const  createAccountArgs = {
+            email: "",
+            password: "",
+            role: 0,
+        }
         it("should fail if user exists", async () => {
             usersRepository.findOne.mockResolvedValue({
                 id: 1,
-                email: "asdad"
+                email: ""
             });
-            const result = await service.createAccount({
-                email: "",
-                password: "",
-                role: 0,
-            });
+            const result = await service.createAccount(createAccountArgs);
             expect(result).toMatchObject({ 
                 ok: false, 
                 error: 'There is a user with that email already'
+            });
+        });
+        it('should create a new user', async () => {
+            usersRepository.findOne.mockResolvedValue(undefined);
+            usersRepository.create.mockReturnValue(createAccountArgs);
+            usersRepository.save.mockResolvedValue(createAccountArgs);
+            verificationsRepository.create.mockReturnValue({user: createAccountArgs});
+            verificationsRepository.save.mockResolvedValue({
+                code: 'code',
             })
-        })
+            const result = await service.createAccount(createAccountArgs);
+            expect(usersRepository.create).toHaveBeenCalledTimes(1);
+            expect(usersRepository.create).toHaveBeenCalledWith(createAccountArgs);
+
+            expect(usersRepository.save).toHaveBeenCalledTimes(1);
+            expect(usersRepository.save).toHaveBeenCalledWith(createAccountArgs);
+
+            expect(verificationsRepository.create).toHaveBeenCalledTimes(1)
+            expect(verificationsRepository.create).toHaveBeenCalledWith({user: createAccountArgs})
+
+            expect(verificationsRepository.save).toHaveBeenCalledTimes(1);
+            expect(verificationsRepository.save).toHaveBeenCalledWith({user: createAccountArgs});
+
+            expect(mailService.sendVerificationEmail).toHaveBeenCalledTimes(1)
+            expect(mailService.sendVerificationEmail).toHaveBeenCalledWith(expect.any(String), expect.any(String),)
+            expect(result).toEqual({ ok: true })
+        });
     })
     it.todo('login')
     it.todo('findById')
