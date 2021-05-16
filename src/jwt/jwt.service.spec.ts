a8e58e5e76847ca6d41db4217db10b7348fb835e
Yunseonghwan <sunghwan0208@naver.com>
2021-05-16 23:51:07 +0900

    jwt test

---
+++ src/jwt/jwt.service.spec.ts
@@ -0,0 +1,24 @@
+import { Test } from "@nestjs/testing";
+import { CONFIG_OPTIONS } from "src/common/common.constant";
+import { JwtService } from "./jwt.service";
+
+const TEST_KEY = "testKey";
+
+describe("JwtService", () => {
+  let service: JwtService;
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        JwtService,
+        {
+          provide: CONFIG_OPTIONS,
+          useValue: { privateKey: TEST_KEY },
+        },
+      ],
+    }).compile();
+    service = module.get<JwtService>(JwtService);
+  });
+  it("should be defined", () => {
+    expect(service).toBeDefined();
+  });
+});
